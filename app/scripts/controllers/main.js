7d136e014ba9805fcb5c520c2272bbb6874f4081
LasaleFamine <alessio.occhipinti@gmail.com>
2016-03-05 13:48:15 +0100

    almost finish

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,11 +10,14 @@
 angular.module('ngPgspeedApp')
   .controller('MainCtrl', function ($scope, pageSpeed) {
     //$scope.site = { 'url': 'http://www.example.com' };
-    $scope.results = {};
-    $scope.gaugeObject = {};
-    $scope.gaugeObject.type = "Gauge";
-    $scope.ruleImpact = [];
 
+    $scope.results = {};    
+    $scope.ruleImpact = [];
+    $scope.iconImpact = [];
+
+    // Define vars for gaugeObj
+    $scope.gaugeObject = {};
+    $scope.gaugeObject.type = 'Gauge';
     $scope.gaugeObject.options = {
       width: 400,
       height: 120,
@@ -25,58 +28,95 @@ angular.module('ngPgspeedApp')
       minorTicks: 5
     };
 
+    // Define vars for chartObj
+    var chartRows = [];
+    var chartCols =  [
+    {
+		'id': 'type',
+		'label': 'Type',
+		'type': 'string',
+		'p': {}
+	},
+	{
+		'id': 'value',
+		'label': 'value',
+		'type': 'number',
+		'p': {}	
+	}];
+
+
+
+	/**
+	 * @submit
+	 *	
+	 */
     $scope.submit = function() {
+
+    	$scope.results = {}; 
+
+    	//console.log($scope.site);
+    	if(angular.isUndefined($scope.site)) {
+    		$scope.results = [];        		
+    		$scope.results.error = true;        		  		
+    		$scope.results.msg = 'You must fill the form with a website';
+    		return
+    	}
+
+    	// Start loading bar
     	$scope.loading = true;
-    	$scope.results.msg = "";
-    	var summ = "";
+    	// Init obj results with an empty msg
+    	$scope.results.msg = '';
+
+
+    	// === Call the PageSpeed Service === //
     	pageSpeed.getPageSpeed($scope.site).then(function(data){
+    		console.log('=========== DATA =========');
         	console.log(data);
-        	if(!data.status && !data.code) {
+        	console.log('=========== DATA =========');
+
+        	// If data OK
+        	if(!data.status && !data.code && !angular.equals({}, data)) {
 
         		// Setup results object
         		$scope.results.pageStats = data.pageStats;
         		$scope.results.title = data.title;
         		$scope.results.id = data.id;
         		$scope.results.responseCode = data.responseCode;
+
         		// Init the formattedResults object
         		$scope.results.formattedResults = {};
 
-        		// =================== FOREACH "ruleResults" ====================== //
+        		// =================== FOREACH 'ruleResults' ====================== //
         		angular.forEach(data.formattedResults.ruleResults, function(value, key) {
-        			// Init object for formatted result with "key" value
+        			// Init object for formatted result with 'key' value
         			// Add basics rules
         			$scope.results.formattedResults[key] = {};
     				$scope.results.formattedResults[key].ruleName = value.localizedRuleName;
     				$scope.results.formattedResults[key].ruleImpact = value.ruleImpact;
 
-    				// If "summary" and "args" are present set the args
+    				// If 'summary' and 'args' are present set the args
         			if(value.summary && value.summary.args) {
-        				// Could be a link    					
-        				var linkInit = '<a href="' + value.summary.args[0].value + '">';
-        				if(value.summary.format.indexOf("{{BEGIN_LINK}}") !== -1) {
-		        			summ = value.summary.format.replace(/{{BEGIN_LINK}}/g, linkInit);
-		        			summ = summ.replace(/{{END_LINK}}/g, '</a>');
-		        		// Could be a number of redirects
-        				} else if(value.summary.format.indexOf("{{NUM_REDIRECTS}}") !== -1) {
-        					summ = value.summary.format.replace(/{{NUM_REDIRECTS}}/g, "<strong>" + value.summary.args[0].value + "</strong>");
-        				}
-        				//======== Could be something else? ========//
+        				parseSummary(value, $scope.results.formattedResults[key]);       				
 
-        				// Push the new summary inside the "key" formatted result
-        				$scope.results.formattedResults[key].summary = summ;	
-
-        			// Else if only "summary" is present push only the summary
+        			// Else if only 'summary' is present push only the summary
         			} else if (value.summary) {
         				$scope.results.formattedResults[key].summary = value.summary.format;
         			}
 
 
-        			// Check for wrost "ruleImpact" and put the class
-    				if(parseFloat(value.ruleImpact) < 1.00) {
-    					$scope.ruleImpact[key] = "success";
+        			// Check for wrost 'ruleImpact' and put the class
+    				if(parseFloat(value.ruleImpact) == 0) {
+    					$scope.ruleImpact[key] = 'green darken-2';
+    					$scope.iconImpact[key] = 'verified_user';
+    				} else if(parseFloat(value.ruleImpact) < 1.00) {
+    					$scope.ruleImpact[key] = 'amber darken-2';
+    					$scope.iconImpact[key] = 'info';
     				} else {
-    					$scope.ruleImpact[key] = "danger";
-    				}   
+    					$scope.ruleImpact[key] = 'red darken-3';
+    					$scope.iconImpact[key] = 'warning';
+    				}
+
+    				//$scope.arrRuleImpact.push(parseFloat(value.ruleImpact));
 
     				if(value.urlBlocks) {
     					$scope.results.formattedResults[key].urlBlocks = [];
@@ -84,104 +124,102 @@ angular.module('ngPgspeedApp')
     					parseUrlBlocksUrls(value.urlBlocks, $scope.results.formattedResults[key]);
     				}
 
-    				console.log($scope.results.formattedResults[key]);		
+    				//console.log($scope.results.formattedResults[key].urlBlocks);		
         			
         		});
 
-        		// ============== ENDFOREACH "ruleResults" ================= //
+        		// ============== ENDFOREACH 'ruleResults' ================= //
 
-        		
 
-        		console.log($scope.results);
-
+        		// Define GAUGE obj with SPEED score of the website
         		$scope.gaugeObject.data = [
 			      ['Label', 'Value'],
 			      ['Page Speed', data.ruleGroups.SPEED.score]
 			    ];
 
+			    // call the InitChart func to init the chart
         		$scope.initChart(data.pageStats);
-        		$scope.results.error = false;	
+
+        		// Sett error to false
+        		$scope.results.error = false;
+
+        	// If data response with a status code
         	} else if(data.code) {
-        		$scope.results = [];        		
+        		$scope.results = [];	
         		$scope.results.error = true;
-        		console.log(data.errors[0].message); 		  		
+        		console.log(data.errors[0].message);	  		
         		$scope.results.msg = data.errors[0].message;
+
+        	// Else every error
         	} else {
         		$scope.results = [];        		
         		$scope.results.error = true;        		  		
-        		$scope.results.msg = "Error";
-        	}  	
+        		$scope.results.msg = 'Error';
+        	}
+
+        	// Stop loading bar
         	$scope.loading = false;
     	});
     	
     };
 
-    var chartRows = [];
-    var chartCols =  [
-    {
-		"id": "type",
-		"label": "Type",
-		"type": "string",
-		"p": {}
-	},
-	{
-		"id": "value",
-		"label": "value",
-		"type": "number",
-		"p": {}	
-	}];
+   
 
 
 
 	$scope.initChart = function(pagestat) {
+		// Empty the chart rows
 		chartRows = [];
 
-		console.log(pagestat);
+		// Iterate every PAGESTAT
 		angular.forEach(pagestat, function(value, key) {
-
 			var thisVal = {
-				"c": [
+				'c': [
 					{
-						"v": key
+						'v': key
 					},
 					{
-						"v": parseInt(value)
+						'v': parseInt(value)
 					}
 				]
 			};
 			chartRows.push(thisVal);
 		});
 
+
+		// Init the chart obj with rows and columns
 		$scope.chartObject = {
-		  "type": "PieChart",
-		  "displayed": false,
-		  "data": {
-		    "cols": chartCols,
-		    "rows": chartRows
+		  'type': 'PieChart',
+		  'displayed': false,
+		  'data': {
+		    'cols': chartCols,
+		    'rows': chartRows
 		  },
-		  "options": {
-		    "title": "Sales per month",
-		    "isStacked": "true",
-		    "fill": 20,
-		    "displayExactValues": true,
-		    "vAxis": {
-		      "title": "Sales unit",
-		      "gridlines": {
-		        "count": 10
+		  'options': {
+		    'title': 'Sales per month',
+		    'isStacked': 'true',
+		    'fill': 20,
+		    'displayExactValues': true,
+		    'vAxis': {
+		      'title': 'Sales unit',
+		      'gridlines': {
+		        'count': 10
 		      }
 		    },
-		    "hAxis": {
-		      "title": "Date"
+		    'hAxis': {
+		      'title': 'Date'
 		    }
 		  },
-		  "formatters": {}
-		};
+		  'formatters': {}
+		};		
 	};
+
+
 
 	function parseUrlBlocksHeader(urlBlocks, $theScope) {
 		var i = 0;
 		var linkInit, size, percentage, response;
-		angular.forEach(urlBlocks, function(value, key) {
+		angular.forEach(urlBlocks, function(value) {
 
 			// DEBUG
 			//console.log(value);
@@ -189,7 +227,7 @@ angular.module('ngPgspeedApp')
 			if(value.header.args) {
 				angular.forEach(value.header.args, function(item) {
 					if(item.key === 'LINK') {
-						linkInit = '<a href="' + item.value + '">';
+						linkInit = '<a target="_blank" href="' + item.value + '">';
 					} else if(item.key === 'SIZE_IN_BYTES') {
 						size = item.value;
 					} else if(item.key === 'PERCENTAGE') {
@@ -202,22 +240,22 @@ angular.module('ngPgspeedApp')
 
 			var head = value.header.format;
 
-			if(value.header.format.indexOf("{{BEGIN_LINK}}") !== -1) {
+			if(value.header.format.indexOf('{{BEGIN_LINK}}') !== -1) {
 				head = head.replace(/{{BEGIN_LINK}}/g, linkInit);
 				head = head.replace(/{{END_LINK}}/g, '</a>');
 			}
 			// Could be a number of redirects
 			
-			if(value.header.format.indexOf("{{SIZE_IN_BYTES}}") !== -1) {
-				head = head.replace(/{{SIZE_IN_BYTES}}/g, "<strong>" + size + "</strong>");
+			if(value.header.format.indexOf('{{SIZE_IN_BYTES}}') !== -1) {
+				head = head.replace(/{{SIZE_IN_BYTES}}/g, '<strong>' + size + '</strong>');
 			}
 
-			if(value.header.format.indexOf("{{PERCENTAGE}}") !== -1) {
-				head = head.replace(/{{PERCENTAGE}}/g, "<strong>" + percentage + "</strong>");
+			if(value.header.format.indexOf('{{PERCENTAGE}}') !== -1) {
+				head = head.replace(/{{PERCENTAGE}}/g, '<strong>' + percentage + '</strong>');
 			}
 
-			if(value.header.format.indexOf("{{RESPONSE_TIME}}") !== -1) {
-				head = head.replace(/{{RESPONSE_TIME}}/g, "<strong>" + response + "</strong>");
+			if(value.header.format.indexOf('{{RESPONSE_TIME}}') !== -1) {
+				head = head.replace(/{{RESPONSE_TIME}}/g, '<strong>' + response + '</strong>');
 			}
 
 			// DEBUG
@@ -234,15 +272,18 @@ angular.module('ngPgspeedApp')
 	function parseUrlBlocksUrls(urlBlocks, $theScope) {
 		var i = 0;
 		var j;
-		var linkInit, size, percentage, response;
+		var linkInit, size, percentage, response, lifetime;
 		var url, redUrl;
 		var head;
 		angular.forEach(urlBlocks, function(urls) {
 			angular.forEach(urls, function(value, key) {
+				// Return if key is header
+				if(key === 'header') {
+					return;
+				}
+				
 
 				$theScope.urlBlocks[i].urls = [];
-
-				//console.log(value);
 
 				//console.log(value.length);
 				if(value.length) {
@@ -252,7 +293,9 @@ angular.module('ngPgspeedApp')
 						head = plusval.result.format;
 
 						angular.forEach(plusval.result.args, function(newPlusval) {
-							if(newPlusval.key === 'URL') {
+							if(newPlusval.key === 'LINK') {
+								linkInit = '<a target="_blank" href="' + newPlusval.value + '">';
+							} else if(newPlusval.key === 'URL') {
 								url = newPlusval.value;
 							} else if(newPlusval.key === 'REDIRECTED_URL') {
 								redUrl = newPlusval.value;
@@ -260,24 +303,40 @@ angular.module('ngPgspeedApp')
 								size = newPlusval.value;
 							} else if(newPlusval.key === 'PERCENTAGE') {
 								percentage = newPlusval.value;
+							}  else if(newPlusval.key === 'LIFETIME') {
+								lifetime = newPlusval.value;
 							}
 						});
+
+						if(plusval.result.format.indexOf('{{BEGIN_LINK}}') !== -1) {
+							head = head.replace(/{{BEGIN_LINK}}/g, linkInit);
+							head = head.replace(/{{END_LINK}}/g, '</a>');
+						}
+
+						if(plusval.result.format.indexOf('{{URL}}') !== -1) {
+							head = head.replace(/{{URL}}/g, '<strong>' + url + '</strong><br>');
+						}
 						
-						if(plusval.result.format.indexOf("{{SIZE_IN_BYTES}}") !== -1) {
-							head = head.replace(/{{SIZE_IN_BYTES}}/g, "<strong>" + size + "</strong>");
+						if(plusval.result.format.indexOf('{{SIZE_IN_BYTES}}') !== -1) {
+							head = head.replace(/{{SIZE_IN_BYTES}}/g, '<strong>' + size + '</strong>');
 						}
 
-						if(plusval.result.format.indexOf("{{REDIRECTED_URL}}") !== -1) {
-							head = head.replace(/{{REDIRECTED_URL}}/g, "<strong>" + redUrl + "</strong>");
+						if(plusval.result.format.indexOf('{{REDIRECTED_URL}}') !== -1) {
+							head = head.replace(/{{REDIRECTED_URL}}/g, '<strong>' + redUrl + '</strong>');
 						}
 
-						if(plusval.result.format.indexOf("{{RESPONSE_TIME}}") !== -1) {
-							head = head.replace(/{{RESPONSE_TIME}}/g, "<strong>" + response + "</strong>");
+						if(plusval.result.format.indexOf('{{RESPONSE_TIME}}') !== -1) {
+							head = head.replace(/{{RESPONSE_TIME}}/g, '<strong>' + response + '</strong>');
 						}
 
-						if(plusval.result.format.indexOf("{{RESPONSE_TIME}}") !== -1) {
-							head = head.replace(/{{RESPONSE_TIME}}/g, "<strong>" + response + "</strong>");
+						if(plusval.result.format.indexOf('{{LIFETIME}}') !== -1) {
+							head = head.replace(/{{LIFETIME}}/g, '<strong>' + lifetime + '</strong>');
 						}
+
+						if(plusval.result.format.indexOf('{{PERCENTAGE}}') !== -1) {
+							head = head.replace(/{{PERCENTAGE}}/g, '<strong>' + percentage + '</strong>');
+						}
+
 						$theScope.urlBlocks[i].urls[j] = head;
 						j++;
 					});
@@ -291,7 +350,7 @@ angular.module('ngPgspeedApp')
 				if(value.args) {
 					angular.forEach(value.args, function(item) {
 						if(item.key === 'LINK') {
-							linkInit = '<a href="' + item.value + '">';
+							linkInit = '<a target="_blank" href="' + item.value + '">';
 						} else if(item.key === 'SIZE_IN_BYTES') {
 							size = item.value;
 						} else if(item.key === 'PERCENTAGE') {
@@ -301,34 +360,71 @@ angular.module('ngPgspeedApp')
 						}
 					});
 
-					if(value.format.indexOf("{{SIZE_IN_BYTES}}") !== -1) {
-						head = head.replace(/{{SIZE_IN_BYTES}}/g, "<strong>" + size + "</strong>");
+					if(value.format.indexOf('{{SIZE_IN_BYTES}}') !== -1) {
+						head = head.replace(/{{SIZE_IN_BYTES}}/g, '<strong>' + size + '</strong>');
 					}
 
-					if(value.format.indexOf("{{PERCENTAGE}}") !== -1) {
-						head = head.replace(/{{PERCENTAGE}}/g, "<strong>" + percentage + "</strong>");
+					if(value.format.indexOf('{{PERCENTAGE}}') !== -1) {
+						head = head.replace(/{{PERCENTAGE}}/g, '<strong>' + percentage + '</strong>');
 					}
 
-					if(value.format.indexOf("{{RESPONSE_TIME}}") !== -1) {
-						head = head.replace(/{{RESPONSE_TIME}}/g, "<strong>" + response + "</strong>");
+					if(value.format.indexOf('{{RESPONSE_TIME}}') !== -1) {
+						head = head.replace(/{{RESPONSE_TIME}}/g, '<strong>' + response + '</strong>');
 					}		
 				}
 				// Put all in var urls[0]
 				$theScope.urlBlocks[i].urls[0] = head;
-
-
 				
 			});
 
 			i++;
 		});
 
-	}
+	} 
 
 
+	function parseSummary(value, $theScope) {
+		var summ = value.summary.format;
+		var linkInit, nScripts, nCss, nRedirects;
+		console.log(value);
+
+		if(value.summary.args) {
+			angular.forEach(value.summary.args, function(item) {
+				if(item.key === 'LINK') {
+					linkInit = '<a target="_blank" href="' + item.value + '">';
+				} else if(item.key === 'NUM_SCRIPTS') {
+					nScripts = item.value;
+				} else if(item.key === 'NUM_CSS') {
+					nCss = item.value;
+				} else if(item.key === 'NUM_REDIRECTS') {
+					nRedirects = item.value;
+				}
+			});
+		}
 
 
-    
+		if(summ.indexOf('{{BEGIN_LINK}}') !== -1) {
+			summ = summ.replace(/{{BEGIN_LINK}}/g, linkInit);
+			summ = summ.replace(/{{END_LINK}}/g, '</a>');
+		// Could be a number of redirects
+		}
+
+		if(summ.indexOf('{{NUM_SCRIPTS}}') !== -1) {
+			summ = summ.replace(/{{NUM_SCRIPTS}}/g, '<strong>' + nScripts + '</strong>');
+		} 
+
+		if(summ.indexOf('{{NUM_CSS}}') !== -1) {
+			summ = summ.replace(/{{NUM_CSS}}/g, '<strong>' + nCss + '</strong>');
+		}
+
+		if(summ.indexOf('{{NUM_REDIRECTS}}') !== -1) {
+			summ = summ.replace(/{{NUM_REDIRECTS}}/g, '<strong>' + nRedirects + '</strong>');
+		}
+		//======== Could be something else? ========//
+
+		// Push the new summary inside the 'key' formatted result
+		$theScope.summary = summ;	
+	} 
 
 	
   });
